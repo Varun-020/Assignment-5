1b6bdd5862d8d606b0c2e25f09cd4a722149dddd
Varun-020 <varunbaba020@gmail.com>
2021-07-08 18:15:03 +0530

    first commit

---
+++ assingmentSolution-5/Solution.js
@@ -0,0 +1,35 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        //1. passthe props to super class
+        super(props);
+        // set the default internal state
+        
+        //2. binding the method with class 
+        this.clickHandler = this.clickHandler.bind(this);
+      this.state = {
+        clicks: 0
+      };
+    }
+    componentDidMount() {
+      this.refs.myComponentDiv.addEventListener('click', this.clickHandler);
+    }
+    componentWillUnmount() {
+      this.refs.myComponentDiv.removeEventListener('click', this.clickHandler);
+    }
+    clickHandler() {
+      this.setState({
+        clicks: this.clicks + 1
+      });
+    }
+    render() {
+      let children = this.props.children;
+      return (
+        <div className="my-component" ref="myComponentDiv">
+        <h2>My Component ({this.state.clicks} clicks})</h2>
+        <h3>{this.props.headerText}</h3>
+      {children}
+      </div>
+      );
+    }
+   }
+   
